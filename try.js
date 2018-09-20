9c96bf24181a6dc5a8af7314297e411ca609c292
yvesdarly26 <yves-darly.mathieu@polymtl.ca>
2018-09-20 12:44:00 -0400

    Modifications

--- try.js
+++ try.js
@@ -1,26 +1,53 @@
+const ask = function(question)
+{const inquirer = require('inquirer'); 
+inquirer.prompt(question).then(function(answers)
+{console.log(answers)});}
 
+let menu = 0;
 //Il y a 8 questions. Donc un tableau par vehicule.
 //A creer une fonction pour storer toutes les questions. 
-
 //Generer le tableau pour chaque vehicule
+ 
+//Menu//
+console.log('\n\n Bienvenue au gestionnaire de bus \n Voulez-vous \n \n 1-Ajouter un véhicule \n 2-Modifier un véhicule \n 3-Supprimer un véhicule\n\n\n');
 
-const donnees = 
-{identification: 0, immatriculation: 0, fabricant: 0, modele: 0, sieges: 0, capaciteDeb: 0, portes: 0, acces:0};
-console.log(donnees);
+//Generer le nombre de vehicule.
+const inquirer = require('inquirer'); 
 
-//Generer le nombre de vehicule. 
-console.log ('Combien de vehicules?');
-const inquirer = require('inquirer');
-const fs = require ('fs');
+const var1 = 
+[{type :'input',
+name:'nbveh',
+ message: 'La flotte'
+}, {type :'input',
+name:'imm',
+ message: 'La plaque'
+},
+{type :'input',
+name:'fab',
+ message: 'Le fabricant'
+},
+{type :'input',
+name:'mod',
+ message: 'Le modele'
+},
+{type :'input',
+name:'sieg',
+ message: 'Le nombre de sieges'
+},{type :'input',
+name:'capDeb',
+ message: 'La capacite Debout'
+},{type :'input',
+name:'prts',
+ message: 'Le nombre de portes'
+},{type :'input',
+name:'acc',
+ message: 'Le nombre dacces' 
+} ];
 
-const question = 
-{type : 'input',
-name:'flotte',
-};
+vehicule = ask(var1);
 
-inquirer.prompt(question).then(function(answers)
- {console.log(answers)});
+console.log(vehicule);
 
 
-
+//Fonction pour classer
 
