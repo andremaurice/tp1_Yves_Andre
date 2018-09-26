8f8170c1ea3627701471597485348d3f8ecb49cb
andremaurice <amyobouet@yahoo.com>
2018-09-26 17:40:43 -0400

    fonction de validation

--- tp1.js
+++ tp1.js
@@ -3,8 +3,10 @@
 
 const ask = function(question)
 {const inquirer = require('inquirer'); 
-inquirer.prompt(question).then(function(answers)
+inquirer.prompt(question).then(function (answers)
 {console.log(answers)});}
+
+
 
 //Array de questions//
 const var0 =
@@ -13,47 +15,107 @@ const var0 =
     {
         type : 'input',
         name : 'identifiant',
-        message : 'Quelle est le numero d_identification  de votre Vehicule'
-           
+        message : 'Quelle est le numero d_identification  de votre Vehicule',
+        validate: function(value) {
+            
+            var valid = !isNaN(parseFloat(value));
+            return valid || "entrer un numéro d'indentification valide";
+          },
+          filter: Number     
+
+       
     },
+
+
     
   {
     type : 'input',
     name : 'nombre_de_places_debout',
-    message : 'Quelle est le nombre de places debout de votre Vehicule'
+    message : 'Quelle est le nombre de places debout de votre Vehicule',
+    validate: function(value) {
+            
+        var valid = !isNaN(parseFloat(value));
+        return valid || "entrer un nombre de places valide";
+      },
+      filter: Number              
+
 }, {
     type : 'input',
     name : 'nombre_de_portes',
-    message : 'Quelle est le nombre de portes de votre Vehicule'
+    message : 'Quelle est le nombre de portes de votre Vehicule',
+    validate: function(value) {
+            
+        var valid = !isNaN(parseFloat(value));
+        return valid || "entrer un nombre de portes valide";
+      },
+      filter: Number   
 },  {
     type : 'input',
     name : 'immatriculation',
-    message : 'Quelle est l_immatriculation de votre Vehicule'
-},
+    message : "Quelle est l'immatriculation de votre Vehicule",
+    validate: function(value) {
+            
+        var valid = !isNaN(parseFloat(value));
+        return valid || "entrer un numéro d'immatriculation valide";
+      },
+      filter: Number  
+    },
  {
     type : 'input',
     name : 'fabricant',
-    message : 'Quelle est le fabricant de votre Vehicule'
+    message : 'Quelle est le fabricant de votre Vehicule',
+    validate: function(value) {
+            
+        var valid =isNaN(parseFloat(value));
+        return valid || "entrer un nom valide";
+      },
+      filter: String 
 },
 {
     type : 'input',
     name : 'modele',
-    message : 'Quelle est le modèle de votre Vehicule'
+    message : 'Quelle est le modèle de votre Vehicule',
+    validate: function(value) {
+            
+        var valid = isNaN(parseFloat(value));
+        return valid || "entrer un modèle valide";
+      },
+      filter: String  
 },
  {
     type : 'input',
-    name : 'nombre_de_sièges',
-    message : 'Quelle est le nombre de sièges de votre Vehicule'
+    name : 'nombre_de_places_assises',
+    message : 'Quelle est le nombre de sièges de votre Vehicule',
+    validate: function(value) {
+            
+        var valid = !isNaN(parseFloat(value));
+        return valid || "entrer un nombre de sièges valide";
+      },
+      filter: Number  
  },
 {
     type : 'input',
     name : 'nombre_de_voies_d_acces',
-    message : 'Quelle est le nombre de voies d_acces de votre Vehicule'
+    message : "Quelle est le nombre de voies d'acces de votre Vehicule",
+    validate: function(value) {
+            
+        var valid = !isNaN(parseFloat(value));
+        return valid || "entrer un nombre de voies d'accès valide";
+      },
+      filter: Number  
 }
-
-
+     
+       
 
   
 ];
 
 vehicule1=ask(var0);
+
+//inquirer.prompt(questions).then(answers => {
+  //  console.log('\nOrder receipt:');
+    //console.log(JSON.stringify(answers, null, '  '));
+  //});
+
+
+
