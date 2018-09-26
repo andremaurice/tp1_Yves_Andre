82348efe767ecb9f7b4fb69df8a3f6be57538031
yvesdarly26 <yves-darly.mathieu@polymtl.ca>
2018-09-26 17:41:49 -0400

    Menu

--- tp1.js
+++ tp1.js
@@ -1,67 +1,104 @@
 
-const fs= require ('fs');
-//Fonction pour ajouter un véhicule//
+var fs = require('fs');
+var inquirer = require('inquirer'); 
 
-//Fonction pour storer//
-const ecrire = function(tableau)
-{const written = JSON.stringify(tableau, null, 2);
-    fs.writeFile('student.json', written, (err) => {
-        if (err) throw err;
-        console.log('Tableau ecrit');
-        });
-};
-const ask = function(question)
-{const inquirer = require('inquirer'); 
-inquirer.prompt(question).then(function(answers)
-{ecrire(answers);
-    console.log(answers)});};
+//**Declaration de variables**//
 
-
-//Array de questions//
-const var0 =
-[
- 
+//Question du menu principal//
+let var0 = 
     {
         type : 'input',
-        name : 'identifiant',
-        message : 'Quelle est le numero d_identification  de votre Vehicule'   
-    },
+        name : 'choix',
+        message : 'Bienvenue au gestionnaire de bus \n\n\n Voulez-vous ajouter un vehicule? (Taper 1) \n Voulez-vous modifier un vehicule? (Taper 2) \n Voulez-vous retirer un vehicule? (Taper 3)\n\n'   
+    };
+    //Question d'ajout de vehicule//
+
+let table1 = [];
+//**Fonctions**//
+
+let ask = function(question)
+{ inquirer.prompt(question).then(function(answers)
+    {
+        if(answers.choix == 1)
+        {ask(var1);}
+            
+        else if (answers.choix == 2)
+        {ask(var0);
+    }
+        else if (answers.choix == 3)
+        {console.log('Tres bon');
+    }
     
-  {
-    type : 'input',
-    name : 'nombre_de_places_debout',
-    message : 'Quelle est le nombre de places debout de votre Vehicule'
-}, {
-    type : 'input',
-    name : 'nombre_de_portes',
-    message : 'Quelle est le nombre de portes de votre Vehicule'
-},  {
-    type : 'input',
-    name : 'immatriculation',
-    message : 'Quelle est l_immatriculation de votre Vehicule'
-},
- {
-    type : 'input',
-    name : 'fabricant',
-    message : 'Quelle est le fabricant de votre Vehicule'
-},
-{
-    type : 'input',
-    name : 'modele',
-    message : 'Quelle est le modèle de votre Vehicule'
-},
- {
-    type : 'input',
-    name : 'nombre_de_sièges',
-    message : 'Quelle est le nombre de sièges de votre Vehicule'
- },
-{
-    type : 'input',
-    name : 'nombre_de_voies_d_acces',
-    message : 'Quelle est le nombre de voies d_acces de votre Vehicule'
-}
+        else
+        {console.log(answers.choix);
+    };
+    })};
 
-  
-];
+let ecrire = function(tableau)
+{let  written = JSON.stringify(tableau, null, 2);
+    fs.writeFile('student.json', written, (err) => {
+        if (err) throw err;
+        console.log('Tableau ecrit').ask;
+        });
+};
 
-veh1=ask(var0);
+let lire = function()
+{fs.readFile('student.json', (err,data) => {
+    if (err) throw err;
+    let student = JSON.parse(data);
+    console.log(student);})};
+
+
+let menu = function(commande)
+{
+    ask(commande);}
+    
+let var1 =
+    [
+     
+        {
+            type : 'input',
+            name : 'identifiant',
+            message : 'Quelle est le numero d_identification  de votre Vehicule'   
+        },
+        
+      {
+        type : 'input',
+        name : 'nombre_de_places_debout',
+        message : 'Quelle est le nombre de places debout de votre Vehicule'
+    }, {
+        type : 'input',
+        name : 'nombre_de_portes',
+        message : 'Quelle est le nombre de portes de votre Vehicule'
+    },  {
+        type : 'input',
+        name : 'immatriculation',
+        message : 'Quelle est l_immatriculation de votre Vehicule'
+    },
+     {
+        type : 'input',
+        name : 'fabricant',
+        message : 'Quelle est le fabricant de votre Vehicule'
+    },
+    {
+        type : 'input',
+        name : 'modele',
+        message : 'Quelle est le modèle de votre Vehicule'
+    },
+     {
+        type : 'input',
+        name : 'nombre_de_sièges',
+        message : 'Quelle est le nombre de sièges de votre Vehicule'
+     },
+    {
+        type : 'input',
+        name : 'nombre_de_voies_d_acces',
+        message : 'Quelle est le nombre de voies d_acces de votre Vehicule'
+    }
+      
+    ];
+
+
+menu(var0);
+
+
